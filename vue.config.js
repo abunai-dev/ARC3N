88c7459fed8220a83127a8b499c08786cc50a29d
Gabriel Gehrig <uhrgw@student.kit.edu>
2023-08-21 17:49:27 +0200

    fixed mistake in vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? "/REPO_NAME/" : "/UncertaintySourceArchive/",
+    publicPath: process.env.NODE_ENV === 'production' ? "/UncertaintySourceArchive/" : "/",
 };
