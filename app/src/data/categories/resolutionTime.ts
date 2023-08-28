af3f7fc446d6d5656064b029ac608abeab3066cd
Gabriel Gehrig <uhrgw@student.kit.edu>
2023-08-28 15:56:47 +0200

    Added 'Create new Uncertainty' form

--- app/src/data/categories/resolutionTime.ts
+++ app/src/data/categories/resolutionTime.ts
@@ -2,12 +2,13 @@ import type { Category } from "@/util/types/Category"
 import designTime from "../manifestations/resolutionTime/designTime";
 import runTime from "../manifestations/resolutionTime/runTime";
 import requirementsTime from "../manifestations/resolutionTime/requirementsTime";
+import realizationTime from "../manifestations/resolutionTime/realizationTime";
 const resolutionTime : Category = {
     "name": "Resolution Time",
     "definition": "This is a test if the type definition of category location is working",
     "description": "This is a test if the type description of category location is working",
     "exampleScenarios": [],
-    "manifestations": [ designTime, runTime, requirementsTime ],
+    "manifestations": [ designTime, realizationTime, runTime, requirementsTime ],
     "imagePath": "resolutionTime.jpg",
     "discussionUrl": ""
 }
