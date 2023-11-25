fe986024e12f21d81b821e6bb309f9f994164449
Alex | Kronox <alexander_vogt@outlook.de>
2023-11-25 14:53:36 +0100

    improve button style

--- UncertaintySourceArchive/src/components/ButtonComponent.vue
+++ UncertaintySourceArchive/src/components/ButtonComponent.vue
@@ -1,7 +1,20 @@
 <template>
   <div
-    class="w-fit cursor-pointer rounded border border-black border-opacity-10 bg-primary-dark p-1 text-white"
+    class="w-fit cursor-pointer rounded border border-black border-opacity-10 bg-primary-dark p-1 text-white hover:bg-[#425D87] active:bg-[#334869]"
+    :class="{
+      '!cursor-not-allowed bg-[#636D7C] hover:bg-[#636D7C] active:bg-[#636D7C]': disabled
+    }"
   >
     <slot></slot>
   </div>
 </template>
+
+<script setup lang="ts">
+defineProps({
+  disabled: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+</script>
