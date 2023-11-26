aa76030b5b3cf9f99297b63ca18c7dbb49abc2aa
Alex | Kronox <alexander_vogt@outlook.de>
2023-11-26 18:16:51 +0100

    adjust button color

--- UncertaintySourceArchive/src/components/ButtonComponent.vue
+++ UncertaintySourceArchive/src/components/ButtonComponent.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="w-fit cursor-pointer rounded border border-black border-opacity-10 bg-primary-dark p-1 text-white hover:bg-[#425D87] active:bg-[#334869]"
+    class="w-fit cursor-pointer rounded border border-black border-opacity-10 bg-primary-dark p-1 text-white hover:bg-[#596B7D] active:bg-[#334869]"
     :class="{
-      '!cursor-not-allowed bg-[#636D7C] hover:bg-[#636D7C] active:bg-[#636D7C]': disabled
+      '!hover:bg-[#636D7C] !active:bg-[#636D7C] !cursor-not-allowed !bg-[#636D7C]': disabled
     }"
   >
     <slot></slot>
