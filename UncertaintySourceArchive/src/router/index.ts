2286b5c2a40e7f0aff1b706829583f42dcd9599e
Alex | Kronox <alexander_vogt@outlook.de>
2023-11-30 14:46:41 +0100

    home view

--- UncertaintySourceArchive/src/router/index.ts
+++ UncertaintySourceArchive/src/router/index.ts
@@ -3,11 +3,17 @@ import ArchiveView from '@/views/ArchiveView.vue'
 import UncertaintyView from '@/views/wrapper/UncertaintyViewWrapper.vue'
 import CreationViewWrapper from '@/views/wrapper/CreationViewWrapper.vue'
 import ExportView from '@/views/ExportView.vue'
+import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      name: 'home',
+      path: '/',
+      component: HomeView
+    },
     {
       name: 'archive',
       path: '/archive',
--- UncertaintySourceArchive/src/views/HomeView.vue
+++ UncertaintySourceArchive/src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>Main View</div>
+  <main class="flex h-full flex-col items-center justify-center text-center">
+    <h1 class="text-3xl font-bold">ARC3N</h1>
+    <h3 class="text-lg font-bold">ReseARCh ARChive for Software-ARChitectural Uncertainty</h3>
+    <p class="my-5">Text</p>
+    <RouterLink to="/archive">
+      <ButtonComponent>Go to Collection</ButtonComponent>
+    </RouterLink>
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ButtonComponent from '@/components/ButtonComponent.vue'
+import { RouterLink } from 'vue-router'
+</script>
