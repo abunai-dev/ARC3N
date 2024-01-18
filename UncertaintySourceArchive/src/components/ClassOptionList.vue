10cbd824314896f1f9c619b79117832c267ceda2
Alex | Kronox <alexander_vogt@outlook.de>
2024-01-18 16:18:24 +0100

    vertical classification view

--- UncertaintySourceArchive/src/components/ClassOptionList.vue
+++ UncertaintySourceArchive/src/components/ClassOptionList.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="flex min-h-[24px]">
+  <div class="flex min-h-[24px]" :class="{ 'flex-col': listOrientation == 'vertical' }">
     <div
       v-for="option in categories[category].options"
       :key="option"
-      class="group flex flex-grow items-center justify-center space-x-2 bg-primary-50 duration-0 first:rounded-l-full last:rounded-r-full dark:bg-primary-950"
+      class="group flex flex-grow items-center space-x-2 bg-primary-50 duration-0 dark:bg-primary-950"
       :class="{
         '!bg-primary-300 dark:!bg-primary-700': option == selectedOption,
-        'hover:bg-primary-200 dark:hover:bg-primary-800': option != selectedOption
+        'hover:bg-primary-200 dark:hover:bg-primary-800':
+          option != selectedOption && listOrientation == 'horizontal',
+        'justify-center first:rounded-l-full last:rounded-r-full': listOrientation == 'horizontal',
+        '!bg-primary-100 pl-3 first:rounded-t-lg last:rounded-b-lg dark:!bg-primary-900':
+          listOrientation == 'vertical'
       }"
     >
       <CategoryIconDisplay :option="option" />
       <p
         class="hidden cursor-default group-hover:flex"
-        :class="{ '!flex': selectedOption == option }"
+        :class="{ '!flex': selectedOption == option || listOrientation == 'vertical' }"
       >
         {{ categoryOptions[option].name }}
       </p>
@@ -34,6 +38,11 @@ defineProps({
   selectedOption: {
     type: String as PropType<CategoryOptionList>,
     required: false
+  },
+  listOrientation: {
+    type: String as PropType<'horizontal' | 'vertical'>,
+    required: false,
+    default: 'horizontal'
   }
 })
 </script>
--- UncertaintySourceArchive/src/views/ClassificationView.vue
+++ UncertaintySourceArchive/src/views/ClassificationView.vue
@@ -1,17 +1,17 @@
 <template>
   <main class="space-y-5">
     <h1 class="text-3xl font-bold">Classification</h1>
-    <div class="grid grid-cols-1 gap-5 md:grid-cols-2">
+    <div class="col-start-2 grid grid-cols-1 gap-5 md:grid-cols-3">
       <SectionComponent
         v-for="category in categoryOrder"
         :key="category"
         :heading="categories[category].name"
         class="col-span-1 col-start-1 row-span-1"
-        :class="`md:col-start-${(categoryOrder.indexOf(category) % 2) + 1}`"
+        :style="{ 'grid-column-start': (categoryOrder.indexOf(category) % 3) + 1 }"
       >
         <div class="space-y-5">
           <p>{{ categories[category].description }}</p>
-          <ClassOptionList :category="category" />
+          <ClassOptionList :category="category" listOrientation="vertical" />
           <RouterLink :to="{ name: 'category', params: { category: category } }">
             <ButtonComponent class="mt-5">More Information</ButtonComponent>
           </RouterLink>
