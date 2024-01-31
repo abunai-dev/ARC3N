4344076b663a81a5e7e64faf83da37ddb37ea883
Alex | Kronox <alexander_vogt@outlook.de>
2024-01-31 23:43:09 +0100

    category view headings

--- UncertaintySourceArchive/src/views/CategoryView.vue
+++ UncertaintySourceArchive/src/views/CategoryView.vue
@@ -19,25 +19,27 @@
       >
         <div class="flex flex-col gap-5 md:flex-row">
           <div class="flex-1">
-            <div class="space-x-2">
+            <div class="space-x-2 text-2xl">
               <CategoryIconDisplay :option="option" />
               <span>{{ categoryOptions[option].name }}</span>
             </div>
             <p class="mt-1">{{ categoryOptions[option].description }}</p>
             <p class="mt-3">{{ categoryOptions[option].exampleText }}</p>
           </div>
-          <UncertaintyTable
-            class="flex-1"
-            :filter="{ [category]: [option] }"
-            :shown-columns="[
-              'id',
-              'name',
-              CategoryList.LOCATION,
-              CategoryList.ARCHITECTURAL_ELEMENT_TYPE,
-              CategoryList.TYPE
-            ]"
-            :max-rows="3"
-          />
+          <div class="flex-1">
+            <h3>Selected examples:</h3>
+            <UncertaintyTable
+              :filter="{ [category]: [option] }"
+              :shown-columns="[
+                'id',
+                'name',
+                CategoryList.LOCATION,
+                CategoryList.ARCHITECTURAL_ELEMENT_TYPE,
+                CategoryList.TYPE
+              ]"
+              :max-rows="3"
+            />
+          </div>
         </div>
       </ContainerComponent>
     </div>
