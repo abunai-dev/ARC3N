e27eb4e8f5ac8358dc1d799217988679075652b5
Alex | Kronox <alexander_vogt@outlook.de>
2024-02-06 13:38:48 +0100

    fix mobile usibility

--- UncertaintySourceArchive/src/views/ClassificationView.vue
+++ UncertaintySourceArchive/src/views/ClassificationView.vue
@@ -1,13 +1,13 @@
 <template>
   <main class="space-y-5">
     <h1 class="text-3xl font-bold">Classification</h1>
-    <div class="col-start-2 grid grid-cols-1 gap-5 md:grid-cols-3">
+    <div class="grid grid-cols-1 gap-5 md:grid-cols-3">
       <RouterLink
         :to="{ name: 'category', params: { category: category } }"
         v-for="category in categoryOrder"
         :key="category"
-        class="col-span-1 col-start-1 row-span-1"
-        :style="{ 'grid-column-start': (categoryOrder.indexOf(category) % 3) + 1 }"
+        class="col-span-1 row-span-1"
+        :class="`grid-start-${(categoryOrder.indexOf(category) % 3) + 1}`"
       >
         <SectionComponent>
           <template #heading>
@@ -31,3 +31,17 @@ import SectionComponent from '@/components/SectionComponent.vue'
 import { categoryOrder, categories } from '@/model/categories/Category'
 import ClassOptionList from '@/components/ClassOptionList.vue'
 </script>
+
+<style scoped>
+@media (min-width: 768px) {
+  .col-start-1 {
+    grid-column-start: 1;
+  }
+  .col-start-2 {
+    grid-column-start: 2;
+  }
+  .col-start-3 {
+    grid-column-start: 3;
+  }
+}
+</style>
